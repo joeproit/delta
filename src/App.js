f62138e6faf3e604a035e38f8fc566b55b8c0d5c
JoePro <git@joeproit.com>
2022-10-29 01:24:13 -0500

    copilot

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My First React App</h1>
+      <h1>React App</h1>
       <p>Welcome :)</p>
-      
+
+      <div className="card">
+        <h2>JoePro</h2>
+        <p className="title">Example</p>
+        <p>Hardknocks</p>
+        <div style={{ margin: '24px 0' }}>
+          <a href="#"><i className="fa fa-dribbble"></i></a>
+          <a href="#"><i className="fa fa-twitter"></i></a>
+          <a href="#"><i className="fa fa-linkedin"></i></a>
+          <a href="#"><i className="fa fa-facebook"></i></a>
+        </div>  
+    </div>
     </div>
   );
 }
